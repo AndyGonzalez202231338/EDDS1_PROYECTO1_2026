fa5bbfbea013bcc13537dd1fac92b23befca12c7
AndyGonzalez202231338 <AndyGonzalez202231338@github.com>
2026-03-19 18:33:52 -0600

    Creacion de CMakeList con carpetas cpps

---
+++ build/CMakeFiles/supermercado.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/AVLTree.cpp" "CMakeFiles/supermercado.dir/src/AVLTree.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/AVLTree.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/BPlusTree.cpp" "CMakeFiles/supermercado.dir/src/BPlusTree.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/BPlusTree.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/BTree.cpp" "CMakeFiles/supermercado.dir/src/BTree.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/BTree.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/Benchmark.cpp" "CMakeFiles/supermercado.dir/src/Benchmark.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/Benchmark.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/CSVLoader.cpp" "CMakeFiles/supermercado.dir/src/CSVLoader.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/CSVLoader.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/Catalog.cpp" "CMakeFiles/supermercado.dir/src/Catalog.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/Catalog.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/LinkedList.cpp" "CMakeFiles/supermercado.dir/src/LinkedList.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/LinkedList.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/Logger.cpp" "CMakeFiles/supermercado.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/Logger.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/SortedLinkedList.cpp" "CMakeFiles/supermercado.dir/src/SortedLinkedList.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/SortedLinkedList.cpp.o.d"
+  "/home/andy/Escritorio/Ingenería/QuintoSemestre/Estructura de datos/Proyecto1/src/main.cpp" "CMakeFiles/supermercado.dir/src/main.cpp.o" "gcc" "CMakeFiles/supermercado.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/supermercado.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/supermercado.dir/src/AVLTree.cpp.o"
+  "CMakeFiles/supermercado.dir/src/AVLTree.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/BPlusTree.cpp.o"
+  "CMakeFiles/supermercado.dir/src/BPlusTree.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/BTree.cpp.o"
+  "CMakeFiles/supermercado.dir/src/BTree.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/Benchmark.cpp.o"
+  "CMakeFiles/supermercado.dir/src/Benchmark.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/CSVLoader.cpp.o"
+  "CMakeFiles/supermercado.dir/src/CSVLoader.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/Catalog.cpp.o"
+  "CMakeFiles/supermercado.dir/src/Catalog.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/LinkedList.cpp.o"
+  "CMakeFiles/supermercado.dir/src/LinkedList.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/Logger.cpp.o"
+  "CMakeFiles/supermercado.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/SortedLinkedList.cpp.o"
+  "CMakeFiles/supermercado.dir/src/SortedLinkedList.cpp.o.d"
+  "CMakeFiles/supermercado.dir/src/main.cpp.o"
+  "CMakeFiles/supermercado.dir/src/main.cpp.o.d"
+  "supermercado"
+  "supermercado.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/supermercado.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
